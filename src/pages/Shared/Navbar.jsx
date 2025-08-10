7a401f5d9f7275194f9c8af834dcd3f6beef29aa
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-08-11 04:17:17 +0600

    navbar login logout button fixed

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import ThemeToggle from "../../components/ThemeToggle";
 import { NavLink } from "react-router";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut, loading } = useContext(AuthContext);
+
   return (
     <nav className="bg-white dark:bg-slate-900 shadow-md">
       <div className="container mx-auto px-6 py-4 flex items-center justify-between">
-
         <h1 className="text-2xl font-bold text-slate-900 dark:text-white">
-          BRACU Research  <span className="text-[#7b1e3c]">Connect</span>
+          BRACU Research <span className="text-[#7b1e3c]">Connect</span>
         </h1>
 
         <ul className="hidden md:flex space-x-8 text-slate-700 dark:text-gray-300 font-medium">
-          <NavLink to='/'>
+          <NavLink to="/">
             <li className="hover:text-[#7b1e3c] transition">Home</li>
           </NavLink>
-          <NavLink to='announcements'><li className="hover:text-[#7b1e3c] transition">Announcement</li></NavLink>
-          <NavLink to='thesis-proposal'><li className="hover:text-[#7b1e3c] transition">Proposal Submission</li></NavLink>
+          <NavLink to="announcements">
+            <li className="hover:text-[#7b1e3c] transition">Announcement</li>
+          </NavLink>
+          <NavLink to="thesis-proposal">
+            <li className="hover:text-[#7b1e3c] transition">
+              Proposal Submission
+            </li>
+          </NavLink>
           <li className="hover:text-[#7b1e3c] transition">Guidelines</li>
           <li className="hover:text-[#7b1e3c] transition">Contact</li>
           <li className="hover:text-[#7b1e3c] transition">About</li>
         </ul>
 
-
         <div className="flex items-center space-x-4">
           <ThemeToggle />
-          <NavLink to='/login'>
-            <button className="px-4 py-2 bg-[#7b1e3c] text-white rounded-lg hover:bg-[#651730] transition">
-                Login
+          {user ? (
+            <button
+              onClick={logOut}
+              disabled={loading}
+              className="px-4 py-2 bg-[#7b1e3c] text-white rounded-lg hover:bg-[#651730] transition disabled:opacity-60"
+            >
+              Logout
             </button>
-          </NavLink>
+          ) : (
+            <NavLink to="/login">
+              <button className="px-4 py-2 bg-[#7b1e3c] text-white rounded-lg hover:bg-[#651730] transition">
+                Login
+              </button>
+            </NavLink>
+          )}
         </div>
       </div>
     </nav>
